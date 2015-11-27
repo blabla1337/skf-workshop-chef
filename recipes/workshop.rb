85fb2c908772a831758da3362d83d6fdb4dec317
gtencate <gtencate@schubergphilis.com>
2015-11-28 00:38:22 +0100

    changed sistro to ubuntu, appears more stable

--- recipes/workshop.rb
+++ recipes/workshop.rb
@@ -8,36 +8,103 @@
 #
 #
 
-# install dependancies 
-%w(gcc.x86_64 git  mysql-server).each do |pkg|
-  package pkg
-end
-
-
-# setup owasp-skf workshop
-bash 'setup owasp-skf workshop' do
-  not_if { ::File.exists? '/home/vagrant/skf-workshop'}
+# install dep
+bash 'install owasp-skf dep' do
+  not_if { ::File.exists? '/home/vagrant/skf-workshop/first-run.txt'}
   code <<-EOH
-    cd /home/vagrant; git clone https://github.com/blabla1337/skf-workshop.git
+
+    apt-get update
+    apt-get install python-software-properties  -y --force-yes
+    add-apt-repository ppa:mapnik/boost
+    add-apt-repository ppa:nginx/stable
+    wget -O - http://dl.hhvm.com/conf/hhvm.gpg.key | sudo apt-key add -
+    echo deb http://dl.hhvm.com/ubuntu precise main | sudo tee /etc/apt/sources.list.d/hhvm.list
+    apt-get update
+    apt-get install nginx -y --force-yes
+    apt-get install hhvm -y --force-yes
+    apt-get install git -y --force-yes
+
+    apt-get install screen vim -y --force-yes
+    debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password password root'
+    debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password_again password root'
+    apt-get install mysql-server -y --force-yes
+    apt-get update
+
+    sudo rm /etc/nginx/sites-enabled/default
+    sudo ln -s /etc/nginx/sites-available/nginx-fastcgi /etc/nginx/sites-enabled/nginx-fastcgi
+
     EOH
 end
 
+#apply hdf conf
+template 'config.hdf' do
+  path '/etc/hhvm/my-config.hdf'
+  source 'config.hdf.erb'
+  owner 'root'
+  group 'root'
+  mode '0644'
+end
+
+#apply php conf
+template 'php.ini' do
+  path '/etc/hhvm/my-php.ini'
+  source 'php.ini.erb'
+  owner 'root'
+  group 'root'
+  mode '0644'
+end
+
+#apply nginx-fastcgi conf
+template 'nginx-fastcgi' do
+  path '/etc/nginx/sites-available/nginx-fastcgi'
+  source 'nginx-fastcgi.erb'
+  owner 'root'
+  group 'root'
+  mode '0644'
+end
+
+# setup owasp-skf workshop
+bash 'setup owasp-skf workshop' do
+  not_if { ::File.exists? '/vagrant/www/skf-workshop/first-run.txt'}
+  code <<-EOH
+    mkdir /vagrant
+    mkdir /vagrant/www
+    cd /vagrant/www; git clone https://github.com/blabla1337/skf-workshop.git
+    EOH
+end
+
+#apply nginx-fastcgi conf
+template 'xhp.php' do
+  path '/vagrant/www/skf-workshop/xhp.php'
+  source 'xhp.php.erb'
+  owner 'root'
+  group 'root'
+  mode '0644'
+end
+
+# setup xhp lib
+bash 'setup xhp lib' do
+  not_if { ::File.exists? '/vagrant/www/skf-workshop/first-run.txt'}
+  code <<-EOH
+    cd /vagrant/www;  git clone https://github.com/facebook/xhp-lib.git
+    EOH
+end
 
 # start server
 bash 'start owasp-skf workshop' do
-  not_if { ::File.exists? '/home/vagrant/skf-workshop/first-run.txt'}
+  not_if { ::File.exists? '/vagrant/www/skf-workshop/first-run.txt'}
   code <<-EOH
-    apachectl restart
+    service nginx restart
     service mysqld start
-    mysql -u root  < /home/vagrant/skf-workshop/demo.sql
-    mysqladmin -u root  password root
-    cd /home/vagrant/skf-workshop; hhvm -m server &
+    echo "create database test" | mysql -u root -proot
+    mysql -u root -proot  < /vagrant/www/skf-workshop/demo.sql
+    cd /vagrant/www/skf-workshop; hhvm -m daemon -c /etc/hhvm/my-php.ini -v Eval.EnableXHP=1 -v Eval.EnableHipHopSyntax=true &
     EOH
 end
 
 # set flag install server
 bash 'start owasp-skf flag' do
   code <<-EOH
-    echo "1"  >> /home/vagrant/skf-workshop/first-run.txt
+    echo "1"  >> /vagrant/www/skf-workshop/first-run.txt
     EOH
 end
